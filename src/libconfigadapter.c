3b611c58cfcb44c0f7a44ffc3062ab6924eb4988
Klaus Wissmann <kw@aw.net>
2019-01-19 22:09:30 +0100

    Added GNU header to all source files

--- src/libconfigadapter.c
+++ src/libconfigadapter.c
@@ -1,3 +1,21 @@
+/*
+ *  logactiond - trigger actions based on logfile contents
+ *  Copyright (C) 2019  Klaus Wissmann
+
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <config.h>
 
 #include <stdio.h>
@@ -258,3 +276,5 @@ include_func(config_t *config, const char *include_dir, const char *path, const 
 	return ((const char **)result);
 }
 
+
+/* vim: set autowrite expandtab: */
--- src/properties.c
+++ src/properties.c
@@ -1,3 +1,21 @@
+/*
+ *  logactiond - trigger actions based on logfile contents
+ *  Copyright (C) 2019  Klaus Wissmann
+
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <config.h>
 
 //#include <regex.h>
@@ -83,3 +101,5 @@ create_property_from_action_token(const char *name, size_t length,
 	return create_property_from_token(name, length, pos, 0);
 }
 
+
+/* vim: set autowrite expandtab: */
