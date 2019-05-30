12b9fd64a6984a23910da024735dd49468a1b9b5
Klaus Wissmann <kw@aw.net>
2019-05-30 08:46:02 +0200

    Updated logcheck rules

--- src/inotify.c
+++ src/inotify.c
@@ -172,7 +172,7 @@ watched_file_created(la_source_t *source)
 {
         assert_source(source);
 
-        la_log(LOG_INFO, "Source \"%s\" - file has ben re-created", source->name);
+        la_log(LOG_INFO, "Source \"%s\" - file has been re-created", source->name);
 
         /* unwatch not necessary in case of a previous IN_DELETE */
         if (source->file)
