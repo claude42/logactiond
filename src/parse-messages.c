10bbfbb3444e957fdfdf5cb043c0fe8f6b9991d8
Klaus Wissmann <kw@aw.net>
2019-09-26 15:12:54 +0200

    Fixed stupid typo

--- src/parse-messages.c
+++ src/parse-messages.c
@@ -87,8 +87,7 @@ parse_add_entry_message(char *message, la_address_t **address, la_rule_t **rule,
                 *rule = find_rule(comma+sizeof(char));
                 if (!*rule)
                 {
-                        la_log_verbose(LOG_ERR, "Unable to find rule in command
-                                        %s!", message);
+                        la_log_verbose(LOG_ERR, "Unable to find rule in command %s!", message);
                         free_address(*address);
                         return false;
                 }
